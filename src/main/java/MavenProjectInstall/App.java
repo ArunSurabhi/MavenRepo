3da035e9c2e1dd5d71fcb01ce12ee6614ed55f08
ArunSurabhi <arun.soman-nair@capgemini.com>
2018-09-26 10:23:04 +0530

    Maven project commited

---
+++ src/main/java/MavenProjectInstall/App.java
@@ -0,0 +1,13 @@
+package MavenProjectInstall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
